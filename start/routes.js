a84ea315028537826e3317b69dc042c74f657c2c
gef12 <gerfersoncoelho@gmail.com>
2019-05-09 08:49:56 -0300

    testando o ssh git

--- start/routes.js
+++ start/routes.js
@@ -21,4 +21,4 @@ Route.get('/', () => {
 })
 
 Route.post('/users', 'UserController.create');
-Route.post('/sessions', 'SessionController.create')
+Route.post('/sessions', 'SessionController.create');
