22f4e047ef30123edba2038e6e2d0e19a4f7626a
gef12 <gerfersoncoelho@gmail.com>
2019-05-09 09:19:14 -0300

    update de relacionamentos dos models

--- app/Models/Property.js
+++ app/Models/Property.js
@@ -4,6 +4,15 @@
 const Model = use('Model')
 
 class Property extends Model {
+  //informando que o imóvel sempre pertence a um usuário ou seja 1 X 1
+  user () {
+    return this.belongsTo('App/Models/User')
+  }
+
+  //o relacionamento que um imóvel possui muitas imagens 1 X N
+  images () {
+    return this.hasMany('App/Models/Image')
+  }
 }
 
 module.exports = Property
--- app/Models/User.js
+++ app/Models/User.js
@@ -34,6 +34,11 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+  //relacionamento  informando que um usuário pode ter muitos imóveis cadastrados 1 X N
+  properties () {
+    return this.hasMany('App/Models/Property')
+  }
 }
 
 module.exports = User
